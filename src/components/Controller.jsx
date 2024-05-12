e349ee98ff28e4c440542498137ddfe6c3c30c1e
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-05-12 18:06:09 +0530

    controller compoenents has been refactored

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import jump_btn from "../Header/Jump-button.gif";
-import auto from "../Header/Auto-button.png";
-import manual from "../Header/Manual-button.png";
-import '../css/Game.css'
+import jumpBtn from "../Header/Jump-button.gif";
+import autoBtn from "../Header/Auto-button.png";
+import manualBtn from "../Header/Manual-button.png";
+import "../css/Game.css";
 
 export default function Controller({
   combo,
@@ -13,35 +13,29 @@ export default function Controller({
   startGame,
   setInputToast,
 }) {
-  const handleGear = () => {
+  const toggleGear = () => {
     setGear(gear === "auto" ? "manual" : "auto");
   };
+
   return (
     <div className="controller">
       <div className="check">
-        <div
-          className={
-            gear === "auto" ? "chances-board" : "chances-board unactive"
-          }
-        >
+        <div className={gear === "auto" ? "chances-board" : "chances-board unactive"}>
           {gear === "auto" ? (
             <input type="text" value={combo} onChange={handleInput} />
           ) : (
             "01"
           )}
         </div>
-        <div className="gear-board" onClick={handleGear}>
-          {gear === "auto" ? (
-            <img className="right" src={auto} />
-          ) : (
-            <img className="left" src={manual} />
-          )}
+        <div className="gear-board" onClick={toggleGear}>
+          <img className={gear === "auto" ? "right" : "left"} src={gear === "auto" ? autoBtn : manualBtn} alt={gear === "auto" ? "Auto" : "Manual"} />
         </div>
       </div>
       {gear === "auto" && (
         <img
           className={click ? "jump-btn unactive" : "jump-btn"}
-          src={jump_btn}
+          src={jumpBtn}
+          alt="Jump Button"
           onClick={startGame}
         />
       )}
